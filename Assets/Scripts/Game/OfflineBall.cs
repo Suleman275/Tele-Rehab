e9d14ac14596ac79d7e90a6d47bd31de4da4c80c
Suleman275 <m.suleman275@gmail.com>
2024-04-04 18:34:30 +0300

    ball is dropped when middle wall is hit

--- Assets/Scripts/Game/OfflineBall.cs
+++ Assets/Scripts/Game/OfflineBall.cs
@@ -11,9 +11,13 @@ public class OfflineBall : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.TryGetComponent<TempHandOffline>(out TempHandOffline hand) && !hand.hasBall) {
+        if (other.gameObject.TryGetComponent<TempHandOffline>(out TempHandOffline hand) && !hand.hasBall) { //if collided with hand and hand does not have a ball
             hand.hasBall = true;
             holdingHand = hand;
+        }
+        else if (other.gameObject.TryGetComponent<OfflineMiddleWall>(out OfflineMiddleWall wall) && holdingHand != null) { //if in hand and collides with wall
+            holdingHand.hasBall = false;
+            holdingHand = null;
         }
     }
 }
