411123ac1dccc6285ee64ff329cf50334efb8bed
Suleman275 <m.suleman275@gmail.com>
2024-04-13 14:52:07 +0300

    relay working

--- Assets/Scripts/Game/TempPlayerOnline.cs
+++ Assets/Scripts/Game/TempPlayerOnline.cs
@@ -5,14 +5,14 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class TempPlayerOnline : NetworkBehaviour {
-    [SerializeField] private GameObject hands;
-
-    public override void OnNetworkSpawn() {
-        if (!IsOwner) {
-            Destroy(this);
-            return;
-        }
-        
-        print(UserDataManager.Instance.userRole);
-    }
+    // [SerializeField] private GameObject hands;
+    //
+    // public override void OnNetworkSpawn() {
+    //     if (!IsOwner) {
+    //         Destroy(this);
+    //         return;
+    //     }
+    //     
+    //     print(UserDataManager.Instance.userRole);
+    // }
 }
